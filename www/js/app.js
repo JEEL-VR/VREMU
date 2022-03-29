5d504fdd41369dd96f75f380a50ecce48a800909
Edgar <edgarsanou@gmail.com>
2022-03-30 00:04:24 +0200

    Envio de datos con todas las excepciones controladas

--- www/js/app.js
+++ www/js/app.js
@@ -1,31 +1,46 @@
 $( document ).ready(function() {
-    //$("#pinText").text(localStorage.getItem("pin"));
-    //$("#tIDText").text(localStorage.getItem("exerciseID"));
     $("#sendButton").click(function(){
-        let pinInput= String(localStorage.getItem("pin"));
-        let vrexerciseid= parseInt(localStorage.getItem("exerciseID"));
-        let passedInput = parseInt($("#passed_items").val());
-        let failedInput = parseInt($("#failed_items").val());
-        let scoreInput = parseInt($("#score").val());
-          $.ajax({
-            url: "https://classvr-room-api.herokuapp.com/api/finish_vr_exercise",
-            type: "POST",
-            dataType: "json",
-            contentType: "application/json",
-            data: JSON.stringify({"pin": pinInput,
-                                  "record": {"passed_items": passedInput,
-                                             "failed_items": failedInput, 
-                                             "grade":scoreInput},
-                                             "VRexerciseID": vrexerciseid}),
-          }).done(function (data) {
-            alert("Se ha enviado correctamente");
-          }).fail(function () {
-            console.log("ERROR: La peticion AJAX no ha salido como se esperaba");
-            alert("ERROR: No se ha podido enviar")
-            alert(localStorage.getItem("pin"));
-            alert(localStorage.getItem("exerciseID"));
-            alert(passedInput+" "+failedInput+" "+scoreInput);
-          });
-        return false;
+      //Getting info
+      let pinInput= String(localStorage.getItem("pin"));
+      let vrexerciseid= parseInt(localStorage.getItem("exerciseID"));
+      let passedInput = parseInt($("#passed_items").val());
+      let failedInput = parseInt($("#failed_items").val());
+      let scoreInput = parseInt($("#score").val());
+      //Check if all fields are completed
+      if(passedInput>0&&failedInput>0&&scoreInput>0){
+        //Ajax call with POST method
+        $.ajax({
+          url: "https://classvr-room-api.herokuapp.com/api/finish_vr_exercise",
+          type: "POST",
+          dataType: "json",
+          contentType: "application/json",
+          data: JSON.stringify({"pin": pinInput,
+                                "record": {"passed_items": passedInput,
+                                            "failed_items": failedInput, 
+                                            "grade":scoreInput},
+                                            "VRexerciseID": vrexerciseid}),
+        }).done(function (data) {
+          //Info alert
+          $('#titleModal').text("Informació");
+          $('#descModal').text("S'han enviat les dades correctament");
+          $('#modal1').modal();
+          $('#modal1').modal('open');
+        }).fail(function () {
+          console.log("ERROR: La peticion AJAX no ha salido como se esperaba");
+          //Fail alert
+          $('#titleModal').text("Error");
+          $('#descModal').text("No s'han pogut enviar les dades.");
+          $('#modal1').modal();
+          $('#modal1').modal('open');
+        });
+      }else{
+        //Fail alert
+        $('#titleModal').text("Error");
+        $('#descModal').text("Has d'omplir tots els camps.");
+        $('#modal1').modal();
+        $('#modal1').modal('open');
+      }
+      
+      return false;
     });
 });
