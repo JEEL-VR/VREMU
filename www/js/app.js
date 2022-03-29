7d8e94041aa3e31178d87f2f7d4a5065ab62c37d
Jose <jrosillonieto@gmail.com>
2022-03-29 23:10:42 +0200

    Correccion de la llamada al metodo POST endpoint finish_vr_exercise

--- www/js/app.js
+++ www/js/app.js
@@ -2,20 +2,21 @@ $( document ).ready(function() {
     //$("#pinText").text(localStorage.getItem("pin"));
     //$("#tIDText").text(localStorage.getItem("exerciseID"));
     $("#sendButton").click(function(){
-        let pinInput=localStorage.getItem("pin");
-        let vrexerciseid=localStorage.getItem("exerciseID");
-        let passedInput = $("#passed_items").val();
-        let failedInput = $("#failed_items").val();
-        let scoreInput = $("#score").val();
+        let pinInput= String(localStorage.getItem("pin"));
+        let vrexerciseid= parseInt(localStorage.getItem("exerciseID"));
+        let passedInput = parseInt($("#passed_items").val());
+        let failedInput = parseInt($("#failed_items").val());
+        let scoreInput = parseInt($("#score").val());
           $.ajax({
-            method: "POST",
             url: "https://classvr-room-api.herokuapp.com/api/finish_vr_exercise",
-            data: {"pin": 7906,
-            "record": {"passed_items":4, 
-                       "failed_items":3, 
-                       "grade":80},
-            "VRexerciseID": 22},
-            dataType: "json"
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({"pin": pinInput,
+                                  "record": {"passed_items": passedInput,
+                                             "failed_items": failedInput, 
+                                             "grade":scoreInput},
+                                             "VRexerciseID": vrexerciseid}),
           }).done(function (data) {
             alert("Se ha enviado correctamente");
           }).fail(function () {
